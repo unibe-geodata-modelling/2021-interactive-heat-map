dfe3f61dc68cc51eaf26a5435507a2d053f989a9
Brian6330 <games@schweigler.org>
2021-06-12 16:39:01 +0200

    Use shape-file along with weather stations

--- src/test.py
+++ src/test.py
@@ -1,16 +1,15 @@
-import data_reader
+# import data_reader
 import matplotlib.pyplot as plt
 
 import pandas as pd
-import descartes
+# import descartes
 import geopandas as gpd
 from shapely.geometry import Point, Polygon
 from geopandas import GeoDataFrame
-
-
+import matplotlib.pyplot as plt
 # all_measurements = data_reader.get_content()
 
-points = []
+# points = []
 # for measurement in all_measurements:
 #     print(measurement)
 
@@ -45,17 +44,24 @@ class Coordinates:
 # plt.scatter(x=x, y=y)
 # plt.show()
 
-# df.head()
-crs = {'init': 'epsg:4326'}
+print("Reading basemap for Berne...")
+bern_map = gpd.read_file('../data/UrbanAtlas_Bern_reduced.shp')
+#The file uses the EPSG:3035 https://spatialreference.org/ref/epsg/etrs89-etrs-laea/
+# fig,ax = plt.subplots(figsize = (15,15))
+# bern_map.plot(ax = ax)
+
+print("Creating geometry...")
 df = pd.read_csv("../data/sample_data.csv")
 geometry = [Point(xy) for xy in zip(df['lon'], df['lat'])]
-print("created geometry")
-geo_df = gpd.GeoDataFrame(df, crs=crs, geometry=geometry)
-print("Created new geo_df")
-fig, ax = plt.subplots(figsize=(15, 15))
+print("Creating new geo_df...")
+geo_df = gpd.GeoDataFrame(df, crs='epsg:4326', geometry=geometry)
+geo_df = geo_df.to_crs("EPSG:3035")
+
 print("Plotting...")
-geo_df.plot(marker='*', color='green', markersize=5)
-plt.legend(prop={'size':15})
+fig, ax = plt.subplots(figsize=(15, 15))
+bern_map.plot(ax=ax, alpha=0.4,color="grey")
+geo_df.plot(ax=ax, marker='*', color='green', markersize=5)
+# plt.legend(prop={'size': 50})
 plt.show()
 
 
