fcbf50f13a5d8d415017d46c2d8c41c9f53da0c6
Brian6330 <games@schweigler.org>
2021-07-16 17:06:12 +0200

    Streamline code to refactor function into its own file, create hourly files

---
+++ src/map_factory.py
@@ -0,0 +1,94 @@
+# Class to create maps based on dataframe with corresponding time filter
+from bokeh.io import output_file, show
+from bokeh.layouts import layout
+from bokeh.models import ColumnDataSource, HoverTool, Div, Dropdown, RadioButtonGroup
+from bokeh.plotting import figure
+from bokeh.tile_providers import get_provider
+
+
+def create_cws_map(df, filtered_columns, suffix):
+    """Reads in a data-frame and exports a html file, containing a bokeh map, placed within the 'docu' folder.
+
+    Parameters
+    ----------
+    df : dataframe
+        A dataframe that must contain the following columns:
+        - geometry
+        - p_id
+        - lon
+        - lat
+        - ta_int
+
+    filtered_columns : boolean vector
+        Contains a boolean vector to trim the dataframe.
+
+    suffix : str
+        A string that will be appended to the title of the plots as well as the html file-names.
+    """
+    # create a Python dict as the basis of your ColumnDataSource
+    data = {'x_values': df[filtered_columns]['geometry'].x,
+            'y_values': df[filtered_columns]['geometry'].y,
+            'p_id': df[filtered_columns]['p_id'],
+            'lon': df[filtered_columns]['lon'],
+            'lat': df[filtered_columns]['lat'],
+            'ta_int': df[filtered_columns]['ta_int']}
+
+    # create a ColumnDataSource by passing the dict
+    source = ColumnDataSource(data=data)
+
+    print("Rendering Bokeh Map...")
+    tile_provider = get_provider(
+        "CARTODBPOSITRON_RETINA")  # Many Provider Variants https://docs.bokeh.org/en/latest/docs/reference/tile_providers.html
+
+    # range bounds supplied in web mercator coordinates
+    p = figure(x_range=(825000, 833000), y_range=(5933000, 5935000),
+               x_axis_type="mercator", y_axis_type="mercator",
+               # Notice that passing x_axis_type="mercator" and y_axis_type="mercator" to figure generates axes with latitude and longitude labels, instead of raw Web Mercator coordinates.
+               x_axis_label="Longitude", y_axis_label="Latitude",
+               sizing_mode="stretch_both")
+    p.add_tile(tile_provider)
+    p.circle(x='x_values', y='y_values', source=source)
+
+    my_hover = HoverTool()  # https://automating-gis-processes.github.io/2016/Lesson5-interactive-map-bokeh.html#adding-interactivity-to-the-map
+    my_hover.tooltips = [('Id', '@p_id'), ('Temperature [C]', '@ta_int'), ('Longitude', '@lon'), ('Latitude', '@lat')]
+    p.add_tools(my_hover)
+
+    div = Div(
+        align="center",
+        text="<h1>Citizen Weather Station: " + suffix + ":00</h1>",
+        sizing_mode="stretch_both"
+    )
+
+    layout_plot = layout([
+        [div],
+        [p]
+    ])
+
+    output_file("../docu/Bern-CWS-Map_" + suffix + ".html")
+    show(layout_plot)
+
+    # LABELS = ["2019-06-26 12:00:00", "2019-06-26 13:00:00", "2019-06-26 14:00:00", "2019-06-26 15:00:00",
+    #           "2019-06-26 16:00:00", "2019-06-26 17:00:00", "2019-06-26 18:00:00", "2019-06-26 19:00:00",
+    #           "2019-06-26 20:00:00", "2019-06-26 21:00:00", "2019-06-26 22:00:00", "2019-06-26 23:00:00", ]
+    # radio_button_group = RadioButtonGroup(labels=LABELS, active=0)
+    # radio_button_group.js_on_click(CustomJS(code="""
+    #     console.log('radio_button_group: active=' + this.active, this.toString())
+    # """))
+
+    # menu = [("26.06.19 12:00", "2019-06-26 12:00:00"), ("26.06.19 13:00", "2019-06-26 13:00:00"),
+    #         ("26.06.19 14:00", "2019-06-26 14:00:00"), ("26.06.19 15:00", "2019-06-26 15:00:00"),
+    #         ("26.06.19 16:00", "2019-06-26 16:00:00"), ("26.06.19 17:00", "2019-06-26 17:00:00"),
+    #         ("26.06.19 18:00", "2019-06-26 18:00:00"), ("26.06.19 19:00", "2019-06-26 19:00:00"),
+    #         ("26.06.19 20:00", "2019-06-26 20:00:00"), ("26.06.19 21:00", "2019-06-26 21:00:00"),
+    #         ("26.06.19 22:00", "2019-06-26 22:00:00"), ("26.06.19 23:00", "2019-06-26 23:00:00"), ]
+    #
+    # dropdown = Dropdown(label="Select the time you wish to display", #https://docs.bokeh.org/en/latest/docs/user_guide/interaction/widgets.html#userguide-interaction-widgets
+    #                     align="center", button_type="primary", menu=menu, sizing_mode="stretch_width"
+    #                     )
+    #
+    # layout = layout([
+    #     [div],
+    #     # [radio_button_group],
+    #     [p],
+    #     [dropdown],
+    # ])
--- src/test.py
+++ src/test.py
@@ -1,15 +1,10 @@
-from bokeh.layouts import layout
-from bokeh.models import ColumnDataSource, Dropdown, RadioButtonGroup, Div
-from bokeh.plotting import figure, output_file, show
-from bokeh.tile_providers import get_provider
-from bokeh.models import HoverTool
 import pandas as pd
 import geopandas as gpd
 from shapely.geometry import Point, Polygon
 import matplotlib.pyplot as plt
 # Unofficial Windows Binaries for Python Extension Packages - https://www.lfd.uci.edu/~gohlke/pythonlibs/
 # How to Install GEOS: https://gis.stackexchange.com/questions/38899/geos-and-shapely-installation-on-windows
-
+from map_factory import create_cws_map
 
 print("Reading basemap for Berne...")
 bern_map = gpd.read_file('../data/UrbanAtlas_Bern_reduced.shp') # It's in EPSG:3035 https://spatialreference.org/ref/epsg/etrs89-etrs-laea/
@@ -21,85 +16,44 @@ fig, ax = plt.subplots(figsize=(15, 15))
 
 print("Creating new geo_df...")
 geo_df = gpd.GeoDataFrame(df, crs='epsg:4326', geometry=geometry)
+
+# Remove columns where temperature is NaN
+invalid_measurements = geo_df['ta_int'] != 'NAN'
+geo_df = geo_df[invalid_measurements]
+
+# Create data for hourly filtering:
+column_22 = geo_df['time'] == '2019-06-26 22:00:00'
+column_23 = geo_df['time'] == '2019-06-26 23:00:00'
+column_00 = geo_df['time'] == '2019-06-27 00:00:00'
+column_01 = geo_df['time'] == '2019-06-27 01:00:00'
+column_02 = geo_df['time'] == '2019-06-27 02:00:00'
+column_03 = geo_df['time'] == '2019-06-27 03:00:00'
+column_04 = geo_df['time'] == '2019-06-27 04:00:00'
+column_05 = geo_df['time'] == '2019-06-27 05:00:00'
+
 geo_df_bokeh = geo_df.to_crs("EPSG:3785")  # https://spatialreference.org/ref/epsg/etrs89-etrs-laea/
 
-geo_df_alternative = geo_df.to_crs("EPSG:3035")
 
 # print("Plotting with pyplot...")
+# geo_df_alternative = geo_df.to_crs("EPSG:3035")
 # bern_map.plot(ax=ax, alpha=0.4, color="grey")
 # geo_df_alternative.plot(ax=ax, marker='*', color='green', markersize=5)
 # plt.legend(prop={'size': 50})
 # plt.show()
 
-print("Setting up Bokeh...")  # Source https://docs.bokeh.org/en/latest/docs/user_guide/geo.html
 
-# create a Python dict as the basis of your ColumnDataSource
-data = {'x_values': geo_df_bokeh['geometry'].x,
-        'y_values': geo_df_bokeh['geometry'].y,
-        'p_id': geo_df_bokeh['p_id'],
-        'ta_int': geo_df_bokeh['ta_int']}
+print("Creating hourly maps...")
+create_cws_map(geo_df_bokeh, column_22, "26.06.19 - 22")
+create_cws_map(geo_df_bokeh, column_23, "26.06.19 - 23")
+create_cws_map(geo_df_bokeh, column_00, "27.06.19 - 00")
+create_cws_map(geo_df_bokeh, column_01, "27.06.19 - 01")
+create_cws_map(geo_df_bokeh, column_02, "27.06.19 - 02")
+create_cws_map(geo_df_bokeh, column_03, "27.06.19 - 03")
+create_cws_map(geo_df_bokeh, column_04, "27.06.19 - 04")
+create_cws_map(geo_df_bokeh, column_05, "27.06.19 - 05")
 
-# create a ColumnDataSource by passing the dict
-source = ColumnDataSource(data=data)
 
-print("Rendering Bokeh Map...")
-tile_provider = get_provider("CARTODBPOSITRON_RETINA")  # Many Provider Variants https://docs.bokeh.org/en/latest/docs/reference/tile_providers.html
-
-# range bounds supplied in web mercator coordinates
-p = figure(x_range=(825000, 833000), y_range=(5933000, 5935000),
-           x_axis_type="mercator", y_axis_type="mercator",  # Notice that passing x_axis_type="mercator" and y_axis_type="mercator" to figure generates axes with latitude and longitude labels, instead of raw Web Mercator coordinates.
-           x_axis_label="Longitude", y_axis_label="Latitude",
-           sizing_mode="stretch_both")
-p.add_tile(tile_provider)
-p.circle(x='x_values', y='y_values', source=source)
-
-my_hover = HoverTool()  # https://automating-gis-processes.github.io/2016/Lesson5-interactive-map-bokeh.html#adding-interactivity-to-the-map
-my_hover.tooltips = [('pid of Loggers', '@p_id'), ('Temp', '@ta_int')]
-p.add_tools(my_hover)
-
-div = Div(
-    align="center",
-    text="""
-        <h1>Low-Cost Measurement Network in Bern:</h1>
-        """,
-    sizing_mode="stretch_both"
-)
-
-# LABELS = ["2019-06-26 12:00:00", "2019-06-26 13:00:00", "2019-06-26 14:00:00", "2019-06-26 15:00:00",
-#           "2019-06-26 16:00:00", "2019-06-26 17:00:00", "2019-06-26 18:00:00", "2019-06-26 19:00:00",
-#           "2019-06-26 20:00:00", "2019-06-26 21:00:00", "2019-06-26 22:00:00", "2019-06-26 23:00:00", ]
-# radio_button_group = RadioButtonGroup(labels=LABELS, active=0)
-# radio_button_group.js_on_click(CustomJS(code="""
-#     console.log('radio_button_group: active=' + this.active, this.toString())
-# """))
-
-menu = [("26.06.19 12:00", "2019-06-26 12:00:00"), ("26.06.19 13:00", "2019-06-26 13:00:00"),
-        ("26.06.19 14:00", "2019-06-26 14:00:00"), ("26.06.19 15:00", "2019-06-26 15:00:00"),
-        ("26.06.19 16:00", "2019-06-26 16:00:00"), ("26.06.19 17:00", "2019-06-26 17:00:00"),
-        ("26.06.19 18:00", "2019-06-26 18:00:00"), ("26.06.19 19:00", "2019-06-26 19:00:00"),
-        ("26.06.19 20:00", "2019-06-26 20:00:00"), ("26.06.19 21:00", "2019-06-26 21:00:00"),
-        ("26.06.19 22:00", "2019-06-26 22:00:00"), ("26.06.19 23:00", "2019-06-26 23:00:00"), ]
-
-dropdown = Dropdown(label="Select the time you wish to display", #https://docs.bokeh.org/en/latest/docs/user_guide/interaction/widgets.html#userguide-interaction-widgets
-                    align="center",
-                    button_type="primary",
-                    menu=menu,
-                    sizing_mode="stretch_width"
-                    )
-
-layout = layout([
-    [div],
-    # [radio_button_group],
-    [p],
-    [dropdown],
-])
-
-output_file("Bern-Map.html")
-show(layout)
-
-# TODO Get working with full dataset
-
-print("done")
+print("Script finished.")
 
 # Make graphs for only the fitting time, orient yourself with gif
 # Create graph Factory function
